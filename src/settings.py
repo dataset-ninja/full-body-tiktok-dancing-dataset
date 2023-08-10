27baaff42c5064d3fe378d76a83345b1e0150dfb
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-08-10 19:43:29 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -15,6 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Full Body TikTok Dancing"
 PROJECT_NAME_FULL: str = "Segmentation Full Body TikTok Dancing Dataset"
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
@@ -85,6 +86,7 @@ def get_settings():
 
     settings = {
         "project_name": PROJECT_NAME,
+        "hide_dataset": HIDE_DATASET,
         "license": LICENSE,
         "applications": APPLICATIONS,
         "category": CATEGORY,
